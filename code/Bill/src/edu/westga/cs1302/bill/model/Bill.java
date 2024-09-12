6657cfc8f142df8a90c1d66519b22d471fd49f74
Jake T Miranda <jmirand3@my.westga.edu>
2024-09-12 15:45:49 -0400

    Added Fixed Lab 4 to Repo

--- code/Bill/src/edu/westga/cs1302/bill/model/Bill.java
+++ code/Bill/src/edu/westga/cs1302/bill/model/Bill.java
@@ -1,28 +1,32 @@
 package edu.westga.cs1302.bill.model;
 
-import java.util.ArrayList;
-
-/** Stores information for a bill.
+/**
+ * Manages a set of BillItems.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class Bill {
-	private ArrayList<BillItem> items;
-	
-	/** Create a new empty Bill
+	public static final int MAX_NUMBER_OF_ITEMS = 3;
+	private BillItem[] items;
+	private int size;
+
+	/**
+	 * Create a new empty Bill
 	 * 
 	 * @precondition none
 	 * @postcondition none
 	 * 
 	 */
 	public Bill() {
-		this.items = new ArrayList<BillItem>();
+		this.items = new BillItem[Bill.MAX_NUMBER_OF_ITEMS];
+		this.size = 0;
 	}
-	
-	/** Adds the item to the bill
+
+	/**
+	 * Adds the item to the bill
 	 * 
-	 * @precondition item != null
+	 * @precondition item != null && size < 5
 	 * @postcondition item is added to the list of items in the bill
 	 * 
 	 * @param item the item to be added to the bill
@@ -31,31 +35,47 @@ public class Bill {
 		if (item == null) {
 			throw new IllegalArgumentException("item must not be null.");
 		}
-		this.items.add(item);
+		if (this.size == Bill.MAX_NUMBER_OF_ITEMS) {
+			throw new IllegalStateException("bill items list is full");
+		}
+		this.items[this.size] = item;
+		this.size++;
 	}
 	
-	/** Return the items in the bill
+	/** Return the number of items in the bill
+	 * 
+	 * @return the number of items in the bill
+	 */
+	public int getSize() {
+		return this.size;
+	}
+
+	/**
+	 * Return the items in the bill
 	 * 
 	 * @return the items in the bill
 	 */
-	public ArrayList<BillItem> getItems() {
+	public BillItem[] getItems() {
 		return this.items;
 	}
-	
-	
-	/** Return the subtotal for the bill
+
+	/**
+	 * Return the subtotal for the bill
 	 * 
 	 * @return the subtotal for the bill
 	 */
 	public double getSubTotal() {
 		double subTotal = 0.0;
 		for (BillItem item : this.items) {
-			subTotal += item.getAmount();
+			if (item != null) {
+				subTotal += item.getAmount();
+			}
 		}
 		return subTotal;
 	}
 
-	/** Return the tax for the bill
+	/**
+	 * Return the tax for the bill
 	 * 
 	 * @return the tax for the bill
 	 */
@@ -63,15 +83,17 @@ public class Bill {
 		return this.getSubTotal() * 0.1;
 	}
 
-	/** Return the tip for the bill
+	/**
+	 * Return the tip for the bill
 	 * 
 	 * @return the tip for the bill
 	 */
 	public double getTip() {
-		return this.getSubTotal()*0.2;
+		return this.getSubTotal() * 0.2;
 	}
 
-	/** Return the total for the bill
+	/**
+	 * Return the total for the bill
 	 * 
 	 * @return the total for the bill
 	 */
--- code/Bill/src/edu/westga/cs1302/bill/view/BillTextifier.java
+++ code/Bill/src/edu/westga/cs1302/bill/view/BillTextifier.java
@@ -3,36 +3,55 @@ package edu.westga.cs1302.bill.view;
 import edu.westga.cs1302.bill.model.Bill;
 import edu.westga.cs1302.bill.model.BillItem;
 
-/** Generates text to summarize a Bill
+/**
+ * Generates text to summarize a Bill
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class BillTextifier {
 
-	/** Return a String summarizing the bill.
+	/**
+	 * Return a String summarizing the bill.
 	 * 
 	 * @precondition none
 	 * @postcondition none
 	 * 
 	 * @return a String summarizing the bill
 	 */
-	public String getText(Bill bill) {
+	public static String getText(Bill bill) {
 		if (bill == null) {
 			throw new IllegalArgumentException("Must provide a valid Bill");
 		}
 		String text = "ITEMS" + System.lineSeparator();
 		for (BillItem item : bill.getItems()) {
-			text += item.getName() + " - " + item.getAmount() + System.lineSeparator();
+			if (item != null) {
+				text += item.getName() + " - " + BillTextifier.convertToCurrency(item.getAmount())
+						+ System.lineSeparator();
+			}
 		}
-		
+
 		text += System.lineSeparator();
-		text += "SUBTOTAL - $" + bill.getSubTotal() + System.lineSeparator();
-		text += "TAX - $" + bill.getTax() + System.lineSeparator();
-		text += "TIP - $" + bill.getTip() + System.lineSeparator();
-		text += "TOTAL - $" + bill.getTotal();
-		
+		text += "SUBTOTAL - " + BillTextifier.convertToCurrency(bill.getSubTotal()) + System.lineSeparator();
+		text += "TAX - " + BillTextifier.convertToCurrency(bill.getTax()) + System.lineSeparator();
+		text += "TIP - " + BillTextifier.convertToCurrency(bill.getTip()) + System.lineSeparator();
+		text += "TOTAL - " + BillTextifier.convertToCurrency(bill.getTotal());
+
 		return text;
 	}
-	
+
+	private static String convertToCurrency(double amount) {
+		String result = "$";
+		int number = ((int) (amount * 100));
+		int dollars = number / 100;
+		result += dollars;
+		result += ".";
+		int pennies = number - (dollars * 100);
+		result += pennies;
+		if (pennies == 0) {
+			result += "0";
+		}
+		return result;
+	}
+
 }
