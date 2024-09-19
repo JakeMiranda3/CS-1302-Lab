af5b194192cf1e554d9502669790dcb8195e0ddc
Jake T Miranda <jmirand3@my.westga.edu>
2024-09-18 23:55:55 -0400

    Static Bill Calculations
    1. Changed the methods of the get subtotal, tax, tip, and total for an Array of BillItem objects in a bill
    	to be static in a seperate class. I did this so it can get called without needing a specific bill object.
    
    2. I havent deleted the original methods in the Bill class as I want to test the static methods and ensure my implmentation is correct.

--- code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,48 @@
 package edu.westga.cs1302.bill.model;
 
 public class BillCalculator {
+	
+	/**
+	 * Return the subtotal for the bill
+	 * 
+	 * @return the subtotal for the bill
+	 */
+	public static double getSubTotal(Bill bill) {
+		double subTotal = 0.0;
+		BillItem[] items = bill.getItems();
+		for (BillItem item : items) {
+			if (item != null) {
+				subTotal += item.getAmount();
+			}
+		}
+		return subTotal;
+	}
+	
+	/**
+	 * Return the tax for the bill
+	 * 
+	 * @return the tax for the bill
+	 */
+	public static double getTax(Bill bill) {
+		return getSubTotal(bill) * 0.1;
+	}
+	
+	/**
+	 * Return the tip for the bill
+	 * 
+	 * @return the tip for the bill
+	 */
+	public double getTip(Bill bill) {
+		return getSubTotal(bill) * 0.2;
+	}
+	
+	/**
+	 * Return the total for the bill
+	 * 
+	 * @return the total for the bill
+	 */
+	public double getTotal(Bill bill) {
+		return getSubTotal(bill) + getTax(bill) + getTip(bill);
+	}
 
 }
--- code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
+++ code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.bill.model.Bill;
+import edu.westga.cs1302.bill.model.BillCalculator;
+import edu.westga.cs1302.bill.model.BillItem;
+
 class TestGetSubtotal {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void test() {
+		Bill tab = new Bill();
+		BillItem item1 = new BillItem("orange", 5);
+		tab.addItem(item1);
+		assertEquals(5,BillCalculator.getSubTotal(tab));
 	}
 
 }
