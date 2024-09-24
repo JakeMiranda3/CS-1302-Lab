4b2ede3851ae99cc5fecae58a28e4d36c45eed73
Jake T Miranda <jmirand3@my.westga.edu>
2024-09-24 00:02:25 -0400

    Adding UI Components for Adding Food
    1. Updated the MainWindow fxml and code behind class to support the folllowing components.
    A. Created a label and textfield for entering the name for a new food.
    B. Created a label and combo box for entering the type for a new food.
    C. Added a button to the gui for submitting information for a new food.
    D. Added a listview to enable the user to select a food from the pantry.

--- code/JavaFX Sample/src/edu/westga/cs1302/project1/view/MainWindow.java
+++ code/JavaFX Sample/src/edu/westga/cs1302/project1/view/MainWindow.java
@@ -1,10 +1,39 @@
 package edu.westga.cs1302.project1.view;
 
-/** Codebehind for the Main Window of the application.
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * Codebehind for the Main Window of the application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
 
+	@FXML
+	private ComboBox<?> foodType;
+
+	@FXML
+	private TextField name;
+
+	@FXML
+	private ListView<?> pantryList;
+
+	@FXML
+	void addFood(ActionEvent event) {
+
+	}
+
+	@FXML
+	void initialize() {
+		assert this.foodType != null : "fx:id=\"foodType\" was not injected: check your FXML file 'MainWindow.fxml'.";
+		assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
+		assert this.pantryList != null
+				: "fx:id=\"pantryList\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+	}
 }
