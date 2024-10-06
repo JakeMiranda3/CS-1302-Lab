3e215482fc4ba9c38f2f7cb7344e1a1691913010
Jake T Miranda <jmirand3@my.westga.edu>
2024-10-06 17:18:14 -0400

    Implementation of Sava Bill Data
    1. In the Bill Persistence Manager class I implemented the functionality of the saveBillData method.
    A. Checked for null and then implemented a try block.
    B. In the try block I have a for each loop to go through and write each bill item name and amount seperated by a comma.
    2. Implemented the saveBillData in the MainWindow.
    A. Did a try block for saving the bill and a catch block to catch an IOException and alert the user with a show and wait if it cant save data to file.

--- code/bill-system (2)/Bill/src/edu/westga/cs1302/bill/model/BillPersistenceManager.java
+++ code/bill-system (2)/Bill/src/edu/westga/cs1302/bill/model/BillPersistenceManager.java
@@ -1,15 +1,20 @@
 package edu.westga.cs1302.bill.model;
 
-/** Supports saving and loading bill data,
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Supports saving and loading bill data,
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class BillPersistenceManager {
-	
+
 	public static final String DATA_FILE = "data.txt";
-	
-	/** Save the bill!
+
+	/**
+	 * Save the bill!
 	 * 
 	 * Writes all bill data to DATA_FILE
 	 * 
@@ -17,16 +22,25 @@ public class BillPersistenceManager {
 	 * @postcondition none
 	 * 
 	 * @param bill the bill to save
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	public static void saveBillData(Bill bill) {
-		
+	public static void saveBillData(Bill bill) throws IOException {
+		if (bill == null) {
+			throw new IllegalArgumentException("Must provide a bill");
+		}
+		try (FileWriter writer = new FileWriter(DATA_FILE)) {
+			writer.write(bill.getServerName() + System.lineSeparator());
+			for (BillItem currItem : bill.getItems()) {
+				writer.write(currItem.getName() + "," + currItem.getAmount() + System.lineSeparator());
+			}
+		}
+
 	}
 
-	/** Load the bill!
+	/**
+	 * Load the bill!
 	 * 
-	 * Reads from DATA_FILE
-	 * File is assumed to use the same format as saveBillData
+	 * Reads from DATA_FILE File is assumed to use the same format as saveBillData
 	 * 
 	 * @precondition none
 	 * @postcondition none
