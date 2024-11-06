f0428c8355b9d5a3a8a66e6628b0a311e2e1ad42
Jake T Miranda <jmirand3@my.westga.edu>
2024-11-06 17:34:15 -0500

    Implemented View Model
    1. Implemented a new class named MainWindowViewModel to follow naming paterns from in class activity.
    2. In the MainWindowViewModel constructor I I initialized 6 variables to correspond to the MainWindow.
    A. I have String and Boolean properties to match the MainWindow functionality
    3. Added getters for each variable so they can be accessed in the view when binding.
    4. Added the generate password method to generate the password according from user input.
    A. Implemented as closly as possible from the generate password method in the MainWindow.
    B. This included try catch blocks to catch any exceptions that can be thrown when generating the password.

---
+++ code/Lab 7/src/edu/westga/cs1302/password_generator/viewmodel/MainWindowViewModel.java
@@ -0,0 +1,121 @@
+package edu.westga.cs1302.password_generator.viewmodel;
+
+import edu.westga.cs1302.password_generator.model.PasswordGenerator;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * The class View Model
+ * 
+ * @author Jake Miranda
+ * @version Fall 2024
+ */
+public class MainWindowViewModel {
+	private StringProperty lengthProperty;
+	private StringProperty outputProperty;
+	private BooleanProperty digitProperty;
+	private BooleanProperty lowerCaseProperty;
+	private BooleanProperty upperCaseProperty;
+	private StringProperty errorText;
+
+	/**
+	 * Instantiates a new main window view model
+	 */
+	public MainWindowViewModel() {
+		this.lengthProperty = new SimpleStringProperty("1");
+		this.outputProperty = new SimpleStringProperty("");
+		this.digitProperty = new SimpleBooleanProperty();
+		this.lowerCaseProperty = new SimpleBooleanProperty();
+		this.upperCaseProperty = new SimpleBooleanProperty();
+		this.errorText = new SimpleStringProperty("Error: ");
+	}
+
+	/**
+	 * Gets the length property.
+	 * 
+	 * @return the lengthProperty.
+	 */
+	public StringProperty lengthProperty() {
+		return this.lengthProperty;
+	}
+
+	/**
+	 * Gets the output property.
+	 * 
+	 * @return the outputProperty.
+	 */
+	public StringProperty outputProperty() {
+		return this.outputProperty;
+	}
+
+	/**
+	 * Gets the digit property.
+	 * 
+	 * @return the digitProperty.
+	 */
+	public BooleanProperty digitProperty() {
+		return this.digitProperty;
+	}
+
+	/**
+	 * Gets the lower case Property.
+	 * 
+	 * @return the lowerCaseProperty.
+	 */
+	public BooleanProperty lowerCaseProperty() {
+		return this.lowerCaseProperty;
+	}
+
+	/**
+	 * Gets the upper case Property.
+	 * 
+	 * @return the upperCaseProperty.
+	 */
+	public BooleanProperty upperCaseProperty() {
+		return this.upperCaseProperty;
+	}
+
+	/**
+	 * Gets the error text property.
+	 * 
+	 * @return the errorText.
+	 */
+	public StringProperty errorText() {
+		return this.errorText;
+	}
+
+	/**
+	 * Generates a password according to the established requirements from the user
+	 */
+	public void generatePassword() {
+		int minimumLength = -1;
+
+		try {
+			minimumLength = Integer.parseInt(this.lengthProperty.getValue());
+		} catch (NumberFormatException numberError) {
+			this.errorText.setValue("Error: Invalid Minimum Length: must be a positive integer, but was "
+					+ this.lengthProperty.getValue());
+		}
+		try {
+			PasswordGenerator password = new PasswordGenerator(minimumLength);
+			if (this.digitProperty.getValue()) {
+				password.setMustHaveAtLeastOneDigit(true);
+			}
+			if (this.upperCaseProperty.getValue()) {
+				password.setMustHaveAtLeastOneUpperCaseLetter(true);
+			}
+			if (this.lowerCaseProperty.getValue()) {
+				password.setMustHaveAtLeastOneLowerCaseLetter(true);
+			}
+			String passwordText = password.generatePassword();
+			this.outputProperty.set(passwordText);
+
+		} catch (IllegalArgumentException invalidLengthError) {
+			this.errorText.setValue("Error: Invalid Minimum Length: " + invalidLengthError.getMessage());
+		}
+
+	}
+
+}
