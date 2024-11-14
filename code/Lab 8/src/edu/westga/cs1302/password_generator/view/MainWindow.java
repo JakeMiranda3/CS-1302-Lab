2ba0465cfcf9843974f6b48d3a6807ebe81ce808
Jake T Miranda <jmirand3@my.westga.edu>
2024-11-13 22:54:41 -0500

    Implemented Input Validation
    1.Added a helper method to set up the listener for validation
    A. Saw that only one validation is needed as there is one text box that the user will be entering text in.
    B. Added the addlistener in the method with a regular expression to make sure the new value matches it.
    C. Added a errorText label for the user when typing the minimum length.

--- code/Lab 8/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 8/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -8,39 +8,71 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/** Codebehind for the MainWindow of the Application.
+/**
+ * Codebehind for the MainWindow of the Application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
 
-    @FXML private CheckBox mustIncludeDigits;
-    @FXML private CheckBox mustIncludeLowerCaseLetters;
-    @FXML private CheckBox mustIncludeUpperCaseLetters;
-    @FXML private TextField minimumLength;
-    @FXML private TextArea output;
-    @FXML private Label errorTextLabel;
-    @FXML private Button generatePasswordButton;
-    
-    private ViewModel vm;
-    
-    @FXML
-    void initialize() {
-    	this.vm = new ViewModel();
-    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
-    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
-    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
-    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
-    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
-    	
-    	this.output.textProperty().bind(this.vm.getPassword());
-    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
-    	
-    	this.generatePasswordButton.setOnAction(
-    			(event) -> { 
-    				this.vm.generatePassword();
-    			} 
-    	);
-    }
+	@FXML
+	private Label errorTextLength;
+
+	@FXML
+	private CheckBox mustIncludeDigits;
+	@FXML
+	private CheckBox mustIncludeLowerCaseLetters;
+	@FXML
+	private CheckBox mustIncludeUpperCaseLetters;
+	@FXML
+	private TextField minimumLength;
+	@FXML
+	private TextArea output;
+	@FXML
+	private Label errorTextLabel;
+	@FXML
+	private Button generatePasswordButton;
+
+	private ViewModel vm;
+
+	@FXML
+	void initialize() {
+		this.vm = new ViewModel();
+		this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
+		this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
+		this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
+		this.minimumLength.setText(this.vm.getMinimumLength().getValue());
+		this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
+
+		this.output.textProperty().bind(this.vm.getPassword());
+		this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+
+		this.generatePasswordButton.setOnAction((event) -> {
+			this.vm.generatePassword();
+		});
+		this.setUpListenersForValidation();
+	}
+
+	private void setUpListenersForValidation() {
+		this.minimumLength.textProperty().addListener((observable, oldValue, newValue) -> {
+			try {
+
+				if (newValue != null) {
+					if (newValue.matches("^[1-9]\\d*")) {
+						this.errorTextLength.setVisible(false);
+
+					} else {
+						this.errorTextLength.setVisible(true);
+					}
+				} else {
+					this.errorTextLength.setVisible(true);
+				}
+			} catch (NumberFormatException error) {
+				this.errorTextLength.setVisible(true);
+			}
+
+		});
+
+	}
 }
