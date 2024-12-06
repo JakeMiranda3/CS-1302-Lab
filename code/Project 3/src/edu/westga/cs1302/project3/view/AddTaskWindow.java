520328f32f43f7a2c2eda11dc2ae3ee959044def
Jake T Miranda <jmirand3@my.westga.edu>
2024-12-06 01:04:17 -0500

    Updated GUI to Display Add Task Window
    1. Updated the MainWindow to display the AddTaskWindow as a modal dialogue when the + button is pressed.
    A. In main added a new TASK_WINDOW_TITLE name and a TASK_GUI_RESOURCE to use in MainWindow.
    B. This allowed me to set up the button to open another window.
    C. This refrenced my new class AddTaskWindow which has a method that sets up binding for view model.
    D. Then put this all together in an event handler using the add task button to open the new window.

--- code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
+++ code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
@@ -1,10 +1,21 @@
 package edu.westga.cs1302.project3.view;
 
-/** Codebehind for the Add Task Window of the application.
+import edu.westga.cs1302.project3.viewmodel.ViewModel;
+
+/**
+ * Codebehind for the Add Task Window of the application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class AddTaskWindow {
+	/**
+	 * Sets up the binding for ViewModel and AddTaskWindow
+	 * 
+	 * @param vm The view Model
+	 */
+	public void bindToVM(ViewModel vm) {
+
+	}
 
 }
--- code/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
+++ code/Project 3/src/edu/westga/cs1302/project3/view/MainWindow.java
@@ -4,15 +4,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import edu.westga.cs1302.project3.Main;
 import edu.westga.cs1302.project3.model.Task;
 import edu.westga.cs1302.project3.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * Codebehind for the Main Window of the application.
@@ -27,6 +34,8 @@ public class MainWindow {
 	private MenuItem loadTask;
 	@FXML
 	private MenuItem saveTask;
+	@FXML
+	private Button addTaskButton;
 
 	@FXML
 	private ViewModel vm;
@@ -37,6 +46,27 @@ public class MainWindow {
 		this.listOfTask.setItems(this.vm.getTasks());
 		this.loadTaskFile();
 		this.saveTaskFile();
+
+		this.addTaskButton.setOnAction((event) -> {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource(Main.TASK_GUI_RESOURCE));
+			try {
+				loader.load();
+				Parent parent = loader.getRoot();
+				Scene scene = new Scene(parent);
+				Stage setAddTaskStage = new Stage();
+				setAddTaskStage.setTitle(Main.TASK_WINDOW_TITLE);
+				setAddTaskStage.setScene(scene);
+				setAddTaskStage.initModality(Modality.APPLICATION_MODAL);
+				AddTaskWindow addTaskCodebehind = (AddTaskWindow) loader.getController();
+				addTaskCodebehind.bindToVM(this.vm);
+				setAddTaskStage.showAndWait();
+			} catch (IOException error) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setContentText("Unable to load add task window. ");
+				alert.showAndWait();
+			}
+		});
 	}
 
 	private void loadTaskFile() {
