5dcbb408f0bf45401c84a5cf2c3c14d6a8289f9b
Jake T Miranda <jmirand3@my.westga.edu>
2024-12-06 01:21:20 -0500

    Updated GUI to Cancel Add Task Window
    1.Updated AddTaskWindow to close itself when the cancel button is pressed.
    2. Did this by adding FXIds to both the Cancel button and Anchor pane.
    3. I then in the bind method used the event handler to call the close window button to close the window when the button is pressed.

--- code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
+++ code/Project 3/src/edu/westga/cs1302/project3/view/AddTaskWindow.java
@@ -1,6 +1,10 @@
 package edu.westga.cs1302.project3.view;
 
 import edu.westga.cs1302.project3.viewmodel.ViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Codebehind for the Add Task Window of the application.
@@ -9,12 +13,23 @@ import edu.westga.cs1302.project3.viewmodel.ViewModel;
  * @version Fall 2024
  */
 public class AddTaskWindow {
+	@FXML
+	private Button closeWindow;
+
+	@FXML
+	private AnchorPane guiTaskPane;
+
 	/**
 	 * Sets up the binding for ViewModel and AddTaskWindow
 	 * 
 	 * @param vm The view Model
 	 */
 	public void bindToVM(ViewModel vm) {
+
+		this.closeWindow.setOnAction((event) -> {
+			Stage stage = (Stage) (this.guiTaskPane).getScene().getWindow();
+			stage.close();
+		});
 
 	}
 
